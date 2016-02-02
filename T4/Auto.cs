fc3216c96f82857a0a7aa53e5c655001adaae42e
K2421 <viivi_2@hotmail.com>
2016-02-02 13:31:58 +0200

    T4

---
+++ T4/Auto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace T4
+{
+    class Auto
+    { //properties 
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Tyres { get; set; }
+        //methods  //Tulostaa ominaisuudet 
+        public void PrintData()
+        {
+            Console.WriteLine("Vehicle properties :");
+            Console.WriteLine("Name : {0}", Name);
+            Console.WriteLine("Speed : {0}", Speed);
+            Console.WriteLine("Tyres : {0}", Tyres);
+        }  // palauttaa kaikki ominaisuudet yhtenä merkkijonona 
+        public override string ToString()
+        {
+            return "Name : " + Name + ", Speed : " + Speed + ", Tyres : " + Tyres;
+        }
+    }
+}
---
+++ T4/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace T4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //create a new vehicle            
+            Auto car = new Auto();
+            car.Name = "Kaara";
+            car.Speed = 80;
+            car.Tyres = 4;
+            car.PrintData();
+            car.ToString();
+            Console.WriteLine(car.ToString());
+            Console.ReadLine();
+        }
+    }
+}
